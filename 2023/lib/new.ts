96acabfc031e957932504fc209044e8d25e85937
uncenter <47499684+uncenter@users.noreply.github.com>
2023-12-01 16:00:01 -0500

    feat(2023): ignore just script errors, write template after input fetch

--- 2023/lib/new.ts
+++ 2023/lib/new.ts
@@ -6,13 +6,6 @@ import { Term } from "./utils";
 const day = Bun.env.AOC_INIT_DAY as string;
 if (!day) Term.error("AOC_INIT_DAY environment variable not set.");
 
-const template = Bun.file(join(import.meta.dir, "resources/template.ts"));
-await mkdir(join(import.meta.dir, "../", day));
-await Bun.write(
-	join(import.meta.dir, "../", day, "index.ts"),
-	(await template.text()).split("\n").slice(1).join("\n")
-);
-
 const res = await fetch(`https://adventofcode.com/2023/day/${day}/input`, {
 	headers: {
 		cookie: `session=${Bun.env.AOC_SESSION}`,
@@ -24,4 +17,16 @@ if (res.status == 404) {
 await Bun.write(
 	join(import.meta.dir, "../", day, "input.txt"),
 	await res.text()
+);
+
+try {
+	await mkdir(join(import.meta.dir, "../", day));
+} catch {
+	Term.error(`Directory for day ${day} already exists.`);
+}
+
+const template = Bun.file(join(import.meta.dir, "resources/template.ts"));
+await Bun.write(
+	join(import.meta.dir, "../", day, "index.ts"),
+	(await template.text()).split("\n").slice(1).join("\n")
 );
