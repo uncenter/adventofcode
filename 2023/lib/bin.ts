67f3763098be88b92cc3d00659eba092730090c8
uncenter <47499684+uncenter@users.noreply.github.com>
2023-12-02 00:10:02 -0500

    feat(2023): aoc package script, move init to cli

--- 2023/lib/bin.ts
+++ 2023/lib/bin.ts
@@ -3,9 +3,10 @@
 import bin from "tiny-bin";
 
 import { join } from "node:path";
+import { mkdir } from "node:fs/promises";
 
-import { Term, range } from "./utils";
 import { Challenge } from "./challenge";
+import { Term, range } from "./utils";
 
 const handle = async (args: string[], method: "test" | "run") => {
 	if (!/^\d(\.\.\d)?$/.test(args[0])) {
@@ -33,6 +34,34 @@ const handle = async (args: string[], method: "test" | "run") => {
 	}
 };
 
+const init = async (day: string) => {
+	const res = await fetch(`https://adventofcode.com/2023/day/${day}/input`, {
+		headers: {
+			cookie: `session=${Bun.env.AOC_SESSION}`,
+		},
+	});
+
+	if (res.status == 404) {
+		Term.error("This challenge has not been released yet!");
+	}
+	try {
+		await mkdir(join(import.meta.dir, "../", day));
+	} catch {
+		Term.error(`Directory for day ${day} already exists.`);
+	}
+
+	await Bun.write(
+		join(import.meta.dir, "../", day, "input.txt"),
+		await res.text()
+	);
+
+	const template = Bun.file(join(import.meta.dir, "resources/template.ts"));
+	await Bun.write(
+		join(import.meta.dir, "../", day, "index.ts"),
+		(await template.text()).split("\n").slice(1).join("\n")
+	);
+};
+
 bin("aoc", "CLI tester and runner for Advent of Code TypeScript solutions")
 	.command("test", "Test a solution")
 	.argument("<range>", "Day of solution to test")
@@ -43,5 +72,10 @@ bin("aoc", "CLI tester and runner for Advent of Code TypeScript solutions")
 	.argument("<range>", "Day of solution to run")
 	.action(async (options, args) => {
 		await handle(args, "run");
+	})
+	.command("init", "Initialize template for given day")
+	.argument("<day>", "Day to initialize")
+	.action(async (options, args) => {
+		await init(args[0]);
 	})
 	.run();
