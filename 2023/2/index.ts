8da1d9b7f35c914f0ca30d31848e3f15f636757b
uncenter <47499684+uncenter@users.noreply.github.com>
2023-12-02 12:31:12 -0500

    feat(2023/2): init and part 1

---
+++ 2023/2/index.ts
@@ -0,0 +1,54 @@
+import { Challenge } from "../lib/challenge";
+
+export const one = (data: string) =>
+	data
+		.split("\n")
+		.map((game) =>
+			game
+				.split(": ")[1]
+				.split("; ")
+				.map((set) =>
+					set
+						.split(", ")
+						.map((cube) => {
+							return {
+								color: cube.split(" ")[1],
+								count: cube.split(" ")[0],
+							};
+						})
+						.reduce(
+							(acc, cube, index, cubes) => {
+								// @ts-expect-error
+								acc[cube.color] = acc[cube.color] + cube.count;
+								return acc;
+							},
+							{
+								red: 0,
+								green: 0,
+								blue: 0,
+							}
+						)
+				)
+				.some((set) => set.red > 12 || set.green > 13 || set.blue > 14)
+				? 0
+				: Number(game.split(": ")[0].split(" ")[1])
+		)
+		.reduce((acc, game, index, games) => acc + game, 0);
+
+export const two = (data: string) => {};
+
+export const challenge = new Challenge({
+	samples: {
+		one: [
+			`Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`,
+			8,
+		],
+	},
+	solutions: {
+		one,
+	},
+});
