984eb7d9f5a59da386cd6863c6cbe001440bc06f
uncenter <47499684+uncenter@users.noreply.github.com>
2023-12-02 00:11:09 -0500

    fix(2023): update init template

--- 2023/lib/resources/template.ts
+++ 2023/lib/resources/template.ts
@@ -6,9 +6,6 @@ export const one = (data: string) => {};
 export const two = (data: string) => {};
 
 const challenge = new Challenge({
-	dir: import.meta.dir,
 	samples: {},
 	solutions: {},
 });
-
-challenge.test();
