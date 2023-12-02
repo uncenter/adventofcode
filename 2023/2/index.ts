f2b9a05be077bce0fdf5156b0d5ac52eadede713
uncenter <47499684+uncenter@users.noreply.github.com>
2023-12-02 12:55:59 -0500

    feat(2023/2): PART TWO!!!!

--- 2023/2/index.ts
+++ 2023/2/index.ts
@@ -13,7 +13,7 @@ export const one = (data: string) =>
 						.map((cube) => {
 							return {
 								color: cube.split(" ")[1],
-								count: cube.split(" ")[0],
+								count: Number(cube.split(" ")[0]),
 							};
 						})
 						.reduce(
@@ -35,7 +35,49 @@ export const one = (data: string) =>
 		)
 		.reduce((acc, game, index, games) => acc + game, 0);
 
-export const two = (data: string) => {};
+export const two = (data: string) =>
+	data
+		.split("\n")
+		.map((game) =>
+			Object.values(
+				game
+					.split(": ")[1]
+					.split("; ")
+					.map((set) =>
+						set
+							.split(", ")
+							.map((cube) => {
+								return {
+									color: cube.split(" ")[1],
+									count: Number(cube.split(" ")[0]),
+								};
+							})
+							.reduce(
+								(acc, cube, index, cubes) => {
+									// @ts-expect-error
+									acc[cube.color] = acc[cube.color] + cube.count;
+									return acc;
+								},
+								{
+									red: 0,
+									green: 0,
+									blue: 0,
+								}
+							)
+					)
+					.reduce(
+						(acc, set, index, sets) => {
+							return {
+								red: set.red > acc.red ? set.red : acc.red,
+								green: set.green > acc.green ? set.green : acc.green,
+								blue: set.blue > acc.blue ? set.blue : acc.blue,
+							};
+						},
+						{ red: 0, green: 0, blue: 0 }
+					)
+			).reduce((acc, game, index, games) => acc * game, 1)
+		)
+		.reduce((acc, game, index, games) => acc + game, 0);
 
 export const challenge = new Challenge({
 	samples: {
@@ -47,8 +89,17 @@ Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`,
 			8,
 		],
+		two: [
+			`Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`,
+			2286,
+		],
 	},
 	solutions: {
 		one,
+		two,
 	},
 });
