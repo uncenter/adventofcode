01c806b80308064a03fb6c3815501eb39e972ca2
uncenter <47499684+uncenter@users.noreply.github.com>
2023-12-04 08:49:29 -0500

    feat(2023/3): init

---
+++ 2023/3/index.ts
@@ -0,0 +1,24 @@
+import { Challenge } from "../lib/challenge";
+
+export const one = (data: string) => {};
+
+export const two = (data: string) => {};
+
+export const challenge = new Challenge({
+	samples: {
+		one: [
+			`467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..`,
+			4361,
+		],
+	},
+	solutions: {},
+});
