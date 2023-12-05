db9cf701e5ef574ad1b98259fd677b40fb718231
uncenter <47499684+uncenter@users.noreply.github.com>
2023-12-05 12:40:03 -0500

    feat(2023/5): init

---
+++ 2023/5/index.ts
@@ -0,0 +1,10 @@
+import { Challenge } from "../lib/challenge";
+
+export const one = (data: string) => {};
+
+export const two = (data: string) => {};
+
+export const challenge = new Challenge({
+	samples: {},
+	solutions: {},
+});
